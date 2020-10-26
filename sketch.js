52997f1c914469884ccbb03becb80769fd01038c
Aditi-Atodaria <72127595+Aditi-Atodaria@users.noreply.github.com>
2020-10-27 01:06:55 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -26,7 +26,8 @@ function setup() {
 	helicopterSprite.scale=0.6
 
 	groundSprite=createSprite(width/2, height-35, width,10);
-	groundSprite.shapeColor=color(255)
+	groundSprite.shapeColor=color(255);
+	groundSprite.visible=false;
 
 
 	engine = Engine.create();
